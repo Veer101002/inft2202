0fb55a9bf528c9a9a070c8e0548bbc4ad64cf20e
Veer101002 <virendrasinhbhikhusinh.chauhan@dcmail.ca>
2025-01-17 13:45:39 -0500

    update add.html

--- ice/src/client/app/animals/list.js
+++ ice/src/client/app/animals/list.js
@@ -6,37 +6,37 @@
     Description: This is list.js page.
 */
 
-console.log('we are on the list page');
+// console.log('we are on the list page');
 
-/* do table stuff */
-const eleEmpty = document.getElementById('empty-message');
-const eleTable = document.getElementById('animal-list');
+// /* do table stuff */
+// const eleEmpty = document.getElementById('empty-message');
+// const eleTable = document.getElementById('animal-list');
 
-const records = animalService.getAnimals();
+// const records = animalService.getAnimals();
 
-if (!records.length) {
-    eleEmpty.classList.remove('d-none');
-    eleTable.classList.add('d-none');
-} else {
-    eleEmpty.classList.add('d-none');
-    eleTable.classList.remove('d-none');
-    drawAnimalTable(records);
-}
-/* 
- * 
- */
-function drawAnimalTable(animals) 
-{
-    for(let animal of animals){
-        const row = eleTable.insertRow();
+// if (!records.length) {
+//     eleEmpty.classList.remove('d-none');
+//     eleTable.classList.add('d-none');
+// } else {
+//     eleEmpty.classList.add('d-none');
+//     eleTable.classList.remove('d-none');
+//     drawAnimalTable(records);
+// }
+// /* 
+//  * 
+//  */
+// function drawAnimalTable(animals) 
+// {
+//     for(let animal of animals){
+//         const row = eleTable.insertRow();
 
-        row.insertcell().textcontent = animal.name;
-        row.insertcell().textcontent = animal.breed;
-        row.insertcell().textcontent = animal.legs;
-        row.insertcell().textcontent = animal.eyes;
-        row.insertcell().textcontent = animal.sound;
+//         row.insertcell().textcontent = animal.name;
+//         row.insertcell().textcontent = animal.breed;
+//         row.insertcell().textcontent = animal.legs;
+//         row.insertcell().textcontent = animal.eyes;
+//         row.insertcell().textcontent = animal.sound;
         
-        const eleBtnCell = row insertCell();
-    }
+//         const eleBtnCell = row insertCell();
+//     }
 
-}
+// }
