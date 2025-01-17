f75c54e652dd75d6a6aa180281f4760e0e856f38
Veer101002 <virendrasinhbhikhusinh.chauhan@dcmail.ca>
2025-01-17 13:42:53 -0500

    data added

---
+++ ice/src/client/app/animals/add.js
@@ -0,0 +1,12 @@
+/*
+    Name: Virendrasinh Chauhan
+    filename: add.js
+    Course: INFT 2202
+    Date: January 13, 2025
+    Description: This is create.js page.
+*/
+
+const formData = new FormData(animalForm);
+
+const animalObject = {};
+formData.forEach(value, key) => {}
---
+++ ice/src/client/app/animals/list.js
@@ -0,0 +1,42 @@
+/*
+    Name: Virendrasinh Chauhan
+    filename: list.js
+    Course: INFT 2202
+    Date: January 17, 2025
+    Description: This is list.js page.
+*/
+
+console.log('we are on the list page');
+
+/* do table stuff */
+const eleEmpty = document.getElementById('empty-message');
+const eleTable = document.getElementById('animal-list');
+
+const records = animalService.getAnimals();
+
+if (!records.length) {
+    eleEmpty.classList.remove('d-none');
+    eleTable.classList.add('d-none');
+} else {
+    eleEmpty.classList.add('d-none');
+    eleTable.classList.remove('d-none');
+    drawAnimalTable(records);
+}
+/* 
+ * 
+ */
+function drawAnimalTable(animals) 
+{
+    for(let animal of animals){
+        const row = eleTable.insertRow();
+
+        row.insertcell().textcontent = animal.name;
+        row.insertcell().textcontent = animal.breed;
+        row.insertcell().textcontent = animal.legs;
+        row.insertcell().textcontent = animal.eyes;
+        row.insertcell().textcontent = animal.sound;
+        
+        const eleBtnCell = row insertCell();
+    }
+
+}
